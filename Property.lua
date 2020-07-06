e58e943a44b80213477986d53645ba16881f3314
Chuzzy <thechuzzyguy@gmail.com>
2020-07-06 16:02:10 +0100

    Fix rent calculation for utility

--- Property.lua
+++ Property.lua
@@ -26,8 +26,9 @@ function Property.new(cost, group, rent_values, improvement_cost)
 end
 
 --- Calculate the rent on this property.
+---@param dice_total integer
 ---@return integer
-function Property:rent()
+function Property:rent(dice_total)
     if not self.owner then
         return 0
     else
@@ -38,7 +39,7 @@ function Property:rent()
             local rent_multiplier = self.improvements + 1 -- Double rent for improved cab co
             return self.rent_values[self.owner.cabCompanyCount()] * rent_multiplier
         elseif self.group == "utility" then
-            return self.rent_values[self.owner.utilityCount()]
+            return dice_total * self.rent_values[self.owner.utilityCount()]
         else
             if self.improvements == -1 then -- If property is mortgaged
                 return 0 -- No rent for mortgaged properties
