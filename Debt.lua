c7ec430c5f031cfafa62345a9ba3bd77e885a457
Chuzzy <thechuzzyguy@gmail.com>
2020-07-06 18:40:40 +0100

    Create Debt.lua

---
+++ Debt.lua
@@ -0,0 +1,27 @@
+--- A debt, representing someone owing someone else money.
+---@class Debt
+---@field debtor Player
+---@field creditor Player
+---@field amount integer
+local Debt = {}
+Debt.__index = Debt
+
+--- Creates a new Debt.
+---@param debtor Player
+---@param creditor Player
+---@param amount integer
+---@return Debt
+function Debt.new(debtor, creditor, amount)
+    local self = setmetatable({}, Debt)
+    self.debtor = debtor
+    self.creditor = creditor
+    if not debtor and not creditor then
+        error("debtor and creditor cannot both be nil", 2)
+    end
+    self.amount = amount
+    return self
+end
+
+function Debt:__tostring()
+    return (self.debtor.name or "The Bank") .. " owes " .. (self.creditor.name or "The Bank") .. " $" .. self.amount
+end
