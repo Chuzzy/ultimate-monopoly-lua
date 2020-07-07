ce1c79508e6ba466b94ade881d622b7462347e98
Chuzzy <thechuzzyguy@gmail.com>
2020-07-07 15:07:04 +0100

    Create ActionCard.lua

---
+++ ActionCard.lua
@@ -0,0 +1,23 @@
+--- An action card: Chance, Community Chest, Travel Voucher or Roll 3.
+---@class ActionCard
+local ActionCard = {}
+ActionCard.__index = ActionCard
+
+--- Create a new Action Card.
+---@param name any
+---@param game any
+---@param action function(function)
+---@return ActionCard
+function ActionCard.new(name, game, action)
+    local self = setmetatable({}, ActionCard)
+    self.name = name
+    self.game = game
+    self.action = action
+    self.owner = nil
+    return self
+end
+
+--- Activate the action card.
+function ActionCard:activate()
+    self.action(self.owner, self.game)
+end
