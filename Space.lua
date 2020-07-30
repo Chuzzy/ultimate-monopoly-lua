b5c7de345e0dc7adc9d9e83ed0d851cfe38c6f62
Chuzzy <thechuzzyguy@gmail.com>
2020-07-30 10:28:40 +0100

    Create Space class

---
+++ Space.lua
@@ -0,0 +1,11 @@
+--- A space on the Ultimate Monopoly board.
+---@class Space
+---@field name string
+---@field game Game
+---@field prevName string
+---@field nextName string
+---@field outerName string
+---@field innerName string
+local Space = {}
+Space.__index = Space
+
