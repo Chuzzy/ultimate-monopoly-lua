0cfbc01025a83821de4e52405571aee3f19802e2
Chuzzy <thechuzzyguy@gmail.com>
2020-08-14 14:48:21 +0100

    Create Names.lua

---
+++ Names.lua
@@ -0,0 +1,144 @@
+-- A bunch of shorthand names so I don't have to keep
+-- retyping the same long string 5 times.
+Names = {
+    -- Middle track properties
+    medit = "Mediterranean Avenue",
+    baltic = "Baltic Avenue",
+
+    oriental = "Oriental Avenue",
+    vermont = "Vermont Avenue",
+    connecticut = "Connecticut Avenue",
+
+    charles = "St. Charles Place",
+    states = "States Avenue",
+    virginia = "Virginia Avenue",
+
+    james = "St. James Place",
+    tennessee = "Tennessee Avenue",
+    newyork = "New York Avenue",
+
+    kentucky = "Kentucky Avenue",
+    indiana = "Indiana Avenue",
+    illinois = "Illinois Avenue",
+
+    atlantic = "Atlantic Avenue",
+    ventnor = "Ventnor Avenue",
+    marvin = "Marvin Gardens",
+
+    pacific = "Pacific Avenue",
+    carolina = "North Carolina Avenue",
+    pennsyl = "Pennsylvania Avenue",
+
+    park = "Park Place",
+    boardwalk = "Boardwalk",
+
+    -- Outer track properties
+    lake = "Lake Street",
+    nicollet = "Nicollet Avenue",
+    hennepin = "Hennepin Avenue",
+
+    esplanade = "Esplanade Avenue",
+    canal = "Canal Street",
+    magazine = "Magazine Street",
+    bourbon = "Bourbon Street",
+
+    katy = "Katy Freeway",
+    westheimer = "Westheimer Road",
+    kirby = "Kirby Drive",
+    cullen = "Cullen Boulevard",
+
+    dekalb = "Dekalb Avenue",
+    andrew = "Andrew Young International Boulevard",
+    decatur = "Decatur Street",
+    peach = "Peachtree Street",
+
+    randolph = "Randolph Street",
+    shore = "Lake Shore Drive",
+    wacker = "Wacker Drive",
+    michigan = "Michigan Avenue",
+
+    south = "South Temple",
+    west = "West Temple",
+    north = "North Temple",
+    square = "Temple Square",
+
+    southst = "South Street",
+    broad = "Broad Street",
+    walnut = "Walnut Street",
+    market = "Market Street",
+
+    mulholland = "Mulholland Drive",
+    ventura = "Ventura Boulevard",
+    rodeo = "Rodeo Drive",
+
+    -- Inner track properties
+    florida = "Florida Avenue",
+    miami = "Miami Avenue",
+    biscayne = "Biscayne Avenue",
+
+    lombard = "Lombard Street",
+    embarca = "The Embarcadero",
+    fisher = "Fisherman's Wharf",
+
+    beacon = "Beacon Street",
+    boylston = "Boylston Street",
+    newbury = "Newbury Street",
+
+    fifth = "Fifth Avenue",
+    madison = "Madison Avenue",
+    wall = "Wall Street",
+
+    -- Utility properties
+    elec = "Electric Company",
+    water = "Water Works",
+    cable = "Cable Company",
+    isp = "Internet Service Provider",
+    trash = "Trash Collector",
+    sewer = "Sewage System",
+    tel = "Telephone Company",
+    gas = "Gas Company",
+
+    -- Transit stations
+    reading = "Reading Railroad",
+    pennyslyrr = "Pennsylvania Railroad",
+    bno = "B&O Railroad",
+    short = "Short Line",
+
+    -- Cab companies
+    checker = "Checker Cab Company",
+    black = "Black & White Cab Company",
+    yellow = "Yellow Cab Company",
+    ute = "Ute Cab Company",
+
+    -- Card spaces
+    chance = "Chance",
+    chest = "Community Chest",
+    bus = "Bus Ticket",
+
+    -- Money spaces
+    go = "Go",
+    bonus = "Bonus",
+    payday = "Pay Day",
+
+    -- Taxes
+    income = "Income Tax",
+    luxury = "Luxury Tax",
+    refund = "Tax Refund",
+
+    -- Other corner spaces
+    jail = "In Jail",
+    visit = "Just Visiting",
+    parking = "Free Parking",
+    malloy = "Go To Jail",
+    holland = "Holland Tunnel",
+    subway = "Subway",
+    stock = "Stock Exchange",
+    roll3 = "Roll Three",
+    squeeze = "Squeeze Play",
+
+    -- Other event spaces
+    auction = "Auction",
+    birthday = "Birthday Gift"
+
+}
+return Names
