769db91d28a9c7401681851622c49882df4036ff
Chuzzy <thechuzzyguy@gmail.com>
2020-08-19 17:47:18 +0100

    Begin Board constructor

--- Board.lua
+++ Board.lua
@@ -5,4 +5,131 @@
 local Board = {}
 Board.__index = Board
 
+Space = require("Space")
+local names = require("Names")
+Ordered = require("Ordered")
 
+function Board.new()
+    local self = setmetatable({}, Board)
+    -- Initialise the spaces. All 111 of them.
+    self.spaces = {}
+
+    --- Creates a forward link between two spaces.
+    ---@param space_name string
+    ---@param after_name string
+    ---@param space_action function
+    ---@param after_action function
+    local function linkConsecutiveSpaces(space_name, after_name, space_action,
+                                         after_action)
+        -- Create a space if space_name doesn't exist yet
+        if not self.spaces[space_name] then
+            self.spaces[space_name] = Space.new(space_name, space_action)
+        end
+        -- Create another space if after_name doesn't exist yet
+        if not self.spaces[after_name] then
+            self.spaces[after_name] = Space.new(after_name, after_action)
+        end
+        -- Make the "after" space point backwards to the current space
+        self.spaces[after_name].prev = self.spaces[space_name]
+        -- Make the current space point forward to the "after" space
+        self.spaces[space_name].next = self.spaces[after_name]
+    end
+
+    local function getChanceId()
+        ChanceSpaceCount = 0
+        ChanceSpaceCount = ChanceSpaceCount + 1
+        return " " .. ChanceSpaceCount
+    end
+
+    local function getChestId()
+        CommunityChestSpaceCount = 0
+        CommunityChestSpaceCount = CommunityChestSpaceCount + 1
+        return " " .. CommunityChestSpaceCount + 1
+    end
+
+    local function getTicketId()
+        BusTicketSpaceCount = 0
+        BusTicketSpaceCount = BusTicketSpaceCount + 1
+        return " " .. BusTicketSpaceCount
+    end
+
+    ---@param track table
+    ---@param name string
+    ---@param amount integer
+    local function createMoneySpace(track, name, amount)
+        track[name] = function(space, player, params)
+            print("Money function: " .. amount)
+        end
+    end
+
+    ---@param track table
+    ---@param name string
+    local function createPropertySpace(track, name)
+        track[name] = function(space, player, params)
+            print("Property function: " .. space)
+        end
+    end
+
+    ---@param track table
+    ---@param name string
+    ---@param is_inner boolean
+    local function createTransitStationSpace(track, name, is_inner)
+        local prefix = is_inner and " Inner" or " Outer"
+        track[name .. prefix] = function(space, player, params)
+            print("Here's your travel voucher, " .. player.name)
+        end
+    end
+
+    ---@param track table
+    ---@param name string
+    local function createCabCompanySpace(track, name)
+        track[name] = function(space, player, params)
+            print("Taxi!")
+        end
+    end
+
+    ---@param track table
+    local function createChestSpace(track)
+        local name = names.chest .. getChestId()
+        track[name] = function(space, player, params)
+            print("Community Chest says...")
+        end
+    end
+
+    ---@param track table
+    local function createChanceSpace(track)
+        local name = names.chest .. getChanceId()
+        track[name] = function(space, player, params)
+            print("Feeling lucky?")
+        end
+    end
+
+    local function createJustVisitingSpace(track)
+        local name = "Just Visiting"
+        track[name] = function(space, player, params)
+            print("Just Visiting")
+        end
+    end
+
+    local function createFreeParkingSpace(track)
+        local name = "Free Parking"
+        track[name] = function(space, player, params)
+            print("Free Parking. Beep beep.")
+        end
+    end
+
+    local function createGoToJailSpace(track)
+        local name = "Go To Jail"
+        track[name] = function(space, player, params)
+            print("GO TO JAIL")
+        end
+    end
+
+    local mid_track = Ordered()
+
+    for key in pairs(mid_track) do print(key) end
+
+    return self
+end
+
+Board.new()
