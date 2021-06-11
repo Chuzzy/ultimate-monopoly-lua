4326aab9630cf6f209533a8ba91f2f1d1bbc6527
Chuzzy <thechuzzyguy@gmail.com>
2021-06-11 20:21:35 +0100

    Create GameState enum

---
+++ GameState.lua
@@ -0,0 +1,8 @@
+GameState = {
+    -- The player is about to move by choosing to roll or using a travel voucher.
+    PREMOVE = 1,
+    -- The dice are being rolled.
+    ROLLING = 2,
+    -- The player is moving.
+    MOVING = 3,
+}
