71d0fe5d5c6de8f395e50c7530804177166ca2d5
Chuzzy <thechuzzyguy@gmail.com>
2021-06-15 20:41:14 +0100

    Add GUIDs of dice

--- GUIDs.lua
+++ GUIDs.lua
@@ -4,4 +4,14 @@ GUIDs = {
     gameboard = "4c85ff",
     --- The metal jail cell.
     jailcell = "0957c5",
+    -- The first white die.
+    normaldie1 = "be0ce9",
+    -- The second white die.
+    normaldie2 = "e403e0",
+    -- The third white die.
+    normaldie3 = "849861",
+    -- The red speed die.
+    speedie = "368ea8",
+    -- The blue voucher die.
+    voucherdie = "3e6cbb"
 }
