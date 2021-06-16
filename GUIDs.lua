d947595fce17e131b43e4a60958ed9f0e5c09259
Chuzzy <thechuzzyguy@gmail.com>
2021-06-16 15:58:37 +0100

    Add playing tokens GUIDs and restructure GUID table

--- GUIDs.lua
+++ GUIDs.lua
@@ -4,14 +4,32 @@ GUIDs = {
     gameboard = "4c85ff",
     --- The metal jail cell.
     jailcell = "0957c5",
+    --- The 5 dice featured in the game.
+    dice = {
     --- The first white die.
-    normaldie1 = "be0ce9",
-    --- The second white die.
-    normaldie2 = "e403e0",
-    --- The third white die.
-    normaldie3 = "849861",
-    --- The red speed die.
-    speedie = "368ea8",
-    --- The blue voucher die.
-    voucherdie = "3e6cbb",
+        normal1 = "be0ce9",
+        --- The second white die.
+        normal2 = "e403e0",
+        --- The third white die.
+        normal3 = "849861",
+        --- The red speed die.
+        speedie = "368ea8",
+        --- The blue voucher die.
+        voucherdie = "3e6cbb"
+    },
+    --- The playing tokens.
+    tokens = {
+        car = "4e290c",
+        horse = "6533ce",
+        ship = "129bf7",
+        thimble = "6b8951",
+        shoe = "c1671b",
+        wheelbarrow = "9fc5b4",
+        train = "a9f769",
+        cannon = "aa09bf",
+        moneybag = "4b4c2f",
+        iron = "2eb4ca",
+        lantern = "b9f65a",
+        hat = "2392b8"
+    },
 }
