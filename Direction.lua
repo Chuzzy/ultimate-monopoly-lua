1aa6d155032e77fb5d4fe532fb00410ef50584e6
Chuzzy <thechuzzyguy@gmail.com>
2021-06-17 04:08:37 +0100

    Add Direction.board_side field

--- Direction.lua
+++ Direction.lua
@@ -2,29 +2,34 @@ Direction = {
     ---@type Direction
     LEFT = {
         vector = {0, 0, 0},
+        board_side = "bottom",
         clockwise = function() return Direction.UP end,
         anticlockwise = function() return Direction.DOWN end
     },
     ---@type Direction
     UP = {
         vector = {0, 90, 0},
+        board_side = "left",
         clockwise = function() return Direction.RIGHT end,
         anticlockwise = function() return Direction.LEFT end
     },
     ---@type Direction
     RIGHT = {
         vector = {0, 180, 0},
+        board_side = "top",
         clockwise = function() return Direction.DOWN end,
         anticlockwise = function() return Direction.UP end
     },
     ---@type Direction
     DOWN = {
         vector = {0, 270, 0},
+        board_side = "right",
         clockwise = function() return Direction.LEFT end,
         anticlockwise = function() return Direction.RIGHT end
     }
 }
 ---@class Direction
 ---@field vector table
+---@field board_side string
 ---@field clockwise function
 ---@field anticlockwise function
