87b77f3af2f3f00044942b85aacc89e53e90b405
Chuzzy <thechuzzyguy@gmail.com>
2021-06-17 02:12:26 +0100

    Add vectors to Direction enum

--- Direction.lua
+++ Direction.lua
@@ -1,17 +1,21 @@
 Direction = {
     LEFT = {
+        vector = {0, 0, 0},
         clockwise = function() return Direction.UP end,
         anticlockwise = function() return Direction.DOWN end
     },
     UP = {
+        vector = {0, 90, 0},
         clockwise = function() return Direction.RIGHT end,
         anticlockwise = function() return Direction.LEFT end
     },
     RIGHT = {
+        vector = {0, 180, 0},
         clockwise = function() return Direction.DOWN end,
         anticlockwise = function() return Direction.UP end
     },
     DOWN = {
+        vector = {0, 270, 0},
         clockwise = function() return Direction.LEFT end,
         anticlockwise = function() return Direction.RIGHT end
     }
