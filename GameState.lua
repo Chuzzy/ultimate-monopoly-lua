1edad579eb17af171e3a65113752853670d5a0ad
Chuzzy <thechuzzyguy@gmail.com>
2021-06-22 19:07:21 +0100

    Add UNBEGUN game state

--- GameState.lua
+++ GameState.lua
@@ -1,5 +1,7 @@
 ---@class GameState
 GameState = {
+    -- The game hasn't started yet.
+    UNBEGUN = 0,
     -- The player is about to move by choosing to roll or using a travel voucher.
     PREMOVE = 1,
     -- The dice are being rolled.
