6d2bcb122e745888837e2122d370559299196adc
Chuzzy <thechuzzyguy@gmail.com>
2021-06-26 19:32:53 +0100

    Revert "Remove Space.action and Space:act"
    This reverts commit 1691458753514e5e771fa9141fe41f4f37def9ed.

--- Space.lua
+++ Space.lua
@@ -1,6 +1,7 @@
 --- A space on the Ultimate Monopoly board.
 ---@class Space
 ---@field name string
+---@field action function
 ---@field transit_type number
 ---@field camera_pos table
 ---@field occupant_positions table
@@ -16,20 +17,29 @@ Space.__index = Space
 
 --- Create a new Space.
 ---@param name string
+---@param action function
 ---@param transit_type number
 ---@param camera_pos table
 ---@param occupant_positions table
 ---@param direction Direction
 ---@param avatar_pos table
 ---@param building_positions table
-function Space.new(name, transit_type, camera_pos, occupant_positions, direction, avatar_pos, building_positions)
+function Space.new(name, transit_type, action, camera_pos, occupant_positions, direction, avatar_pos, building_positions)
     local self = setmetatable({}, Space)
     self.name = name
     self.transit_type = transit_type
+    self.action = action
     self.camera_pos = camera_pos
     self.occupant_positions = occupant_positions
     self.direction = direction
     self.avatar_pos = avatar_pos
     self.building_positions = building_positions
     return self
+end
+
+--- Performs the action on this space.
+---@param player UMPlayer
+---@param params table
+function Space:act(player, params)
+    self.action(self, player, params)
 end
