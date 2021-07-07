6e662279a46535cbe7346c64a59bcda4799d0c3a
Chuzzy <thechuzzyguy@gmail.com>
2021-07-08 00:27:01 +0100

    Move populateInGameObjects

--- InGameObjects.lua
+++ InGameObjects.lua
@@ -1,2 +1,18 @@
 ---@type table<string, any>
 InGameObjects = {}
+
+---Populates the InGameObjects table.
+---@param guids table<string, string> Table of GUIDs.
+---@param directory string Name of the directory.
+function populateInGameObjects(guids, directory)
+    for object_name, object_guid in pairs(guids) do
+        if type(object_guid) == "table" then
+            populateInGameObjects(guids[object_name], object_name)
+        elseif directory then
+            InGameObjects[directory] = InGameObjects[directory] or {}
+            InGameObjects[directory][object_name] = getObjectFromGUID(object_guid)
+        else
+            InGameObjects[object_name] = getObjectFromGUID(object_guid)
+        end
+    end
+end
--- Tabletop.lua
+++ Tabletop.lua
@@ -14,22 +14,6 @@ local mutated_btns = {}
 local player_tokens = {}
 Rigged = {}
 
----Populates the InGameObjects table.
----@param guids table<string, string> Table of GUIDs.
----@param directory string Name of the directory.
-function populateInGameObjects(guids, directory)
-    for object_name, object_guid in pairs(guids) do
-        if type(object_guid) == "table" then
-            populateInGameObjects(guids[object_name], object_name)
-        elseif directory then
-            InGameObjects[directory] = InGameObjects[directory] or {}
-            InGameObjects[directory][object_name] = getObjectFromGUID(object_guid)
-        else
-            InGameObjects[object_name] = getObjectFromGUID(object_guid)
-        end
-    end
-end
-
 function onLoad()
     populateInGameObjects(GUIDs)
     InGameObjects.gameboard.interactable = false
