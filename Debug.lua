d1fa875373e877885f2c3ff5f86d97ce91d98526
Chuzzy <thechuzzyguy@gmail.com>
2021-09-04 17:04:44 +0100

    Flesh out Debug.handOut

--- Debug.lua
+++ Debug.lua
@@ -1,6 +1,29 @@
 Debug = {}
 
----Sells some property to some people.
+---Sells a fixed list of properties to Blue, Green and Yellow.
 function Debug.handOut()
-    UMGame.sellPropertyTo(UMGame.players_by_color.Blue, UMGame.properties[Names.wall])
+    local properties_to_hand_out = {
+        Blue = {
+            Names.fifth,
+            Names.madison,
+            Names.wall,
+        },
+        Yellow = {
+            Names.esplanade,
+            Names.canal,
+            Names.magazine,
+        },
+        Green = {
+            Names.cable,
+            Names.vermont,
+            Names.reading,
+            Names.checker,
+            Names.yellow
+        }
+    }
+    for player_color, properties_for_player in pairs(properties_to_hand_out) do
+        for _, property_name in ipairs(properties_for_player) do
+            UMGame.sellPropertyTo(UMGame.players_by_color[player_color], UMGame.properties[property_name])
+        end
+    end
 end
